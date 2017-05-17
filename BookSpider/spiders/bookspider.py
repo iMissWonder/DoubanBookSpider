e1cb84b4124b18ab8bfcd69b58415db87c435959
iMissWonder <joker528@icloud.com>
2017-05-17 23:41:45 +0800

    Update for novels

--- BookSpider/spiders/bookspider.py
+++ BookSpider/spiders/bookspider.py
@@ -10,20 +10,20 @@ sys.setdefaultencoding('utf8')
 
 class DouBan(CrawlSpider):
     name = 'Book'
-    start_urls = ['https://book.douban.com/top250']
+    start_urls = ['https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4']
     redis_key = "DoubanSpider:start_urls"
     download_delay = 1
 
     def parse(self, response):
         selector = Selector(response)
-        book_urls = selector.xpath("//tr[@class='item']/td[2]/div[@class='pl2']/a/@href").extract()
+        book_urls = selector.xpath("//div[@class='info']/h2/a/@href").extract()
 
         for book in book_urls:
             yield Request(book,callback=self.parse_books)
 
-        nextLink = selector.xpath("//span[contains(@class, 'next')]/a/@href").extract()
+        nextLink = selector.xpath("//span[@class='next']/a/@href").extract()
         if nextLink:
-            nextLink = nextLink[0]
+            nextLink = "https://book.douban.com" + nextLink[0]
             print(nextLink)
             yield Request(nextLink,callback=self.parse)
 
@@ -33,25 +33,31 @@ class DouBan(CrawlSpider):
         bookid = str(re.search('subject/(.*?)/',response.url).group(1))
         title = selector.xpath('//h1/span/text()').extract()
         author = selector.xpath("//div[@id='info']/a[1]/text()").extract()
-        author = author[0].replace(' ','').replace('\n','')
-
+        if author:
+            author = author[0].replace(' ','').replace('\n','')
+        else:
+            author = selector.xpath("//div[@id='info']/span[1]/a/text()").extract()
+            author = author[0].replace(' ','').replace('\n','')
         info = selector.xpath("//div[@id='info']").extract()
         info = ''.join(info).replace(' ','').replace('\n','')
         info = info.decode('utf-8')
-
-        isbn13 = str(re.search('ISBN:</span>(.*?)<br>',info).group(1))
+        isbn13 = re.search('ISBN:</span>(.*?)<br>',info)
+        if isbn13:
+            isbn13 = str(isbn13.group(1))
         publisher =str(re.search(u'\u51fa\u7248\u793e:</span>(.*?)<br>',info).group(1))
         pubdate = str(re.search(u'\u51fa\u7248\u5e74:</span>(.*?)<br>',info).group(1))
-        price = str(re.search(u'\u5b9a\u4ef7:</span>(.*?)<br>',info).group(1))
-        pages = str(re.search(u'\u9875\u6570:</span>(.*?)<br>',info).group(1))
+        price = re.search(u'\u5b9a\u4ef7:</span>(.*?)<br>',info)
+        if price:
+            price = str(price.group(1))
+        pages = re.search(u'\u9875\u6570:</span>(.*?)<br>',info)
+        if pages:
+            pages = str(pages.group(1))
         translator = re.search(u'\u8bd1\u8005:</span>(.*?)<br>', info)
         if translator:
             translator = str(re.search(u'>(.*?)</a>', translator.group(1)).group(1))
         summary = selector.xpath("//div[@class='indent']/span[@class='all hidden']").extract()
         if summary:
-            summary = str(re.search(u'<div class="intro">\n(.*?)</div>',summary[0]).group(1))
-            # need fix this!
-            # summary = str(re.search(u'<p>(.*?)</p>', summary).group(0))
+            summary = re.findall(u'<p>(.*?)</p>', summary[0])
         else:
             summary = selector.xpath("//div[@class='intro']/p/text()").extract()
 
@@ -64,13 +70,31 @@ class DouBan(CrawlSpider):
         lookcount = selector.xpath("//div[@id='collector']/p[contains(@class, 'pl')][2]/a/text()").extract()
         lookcount = ''.join(lookcount)
         lookcount = str(re.search(u'(\d+)',lookcount).group(1))
-        image = response.xpath("//a[@class = 'nbg']/img/@src").extract()
+        image = selector.xpath("//a[@class = 'nbg']/img/@src").extract()
+        tryread = selector.xpath("//div[@class='indent']/div/a/@href").extract()
+
+        short_review_content = selector.xpath("//p[@class='comment-content']/text()").extract()
+        short_review_name = selector.xpath("//span[@class='comment-info']/a/text()").extract()
+        short_review_id = selector.xpath("//span[@class='comment-info']/a/@href").extract()
+        if short_review_id:
+            for index,id in enumerate(short_review_id):
+                short_review_id[index] = str(re.search('https://www.douban.com/people/(.*?)/',id).group(1))
+
+        review_title = selector.xpath("//a[@class='title-link']/text()").extract()
+        review_link = selector.xpath("//a[@class='title-link']/@href").extract()
+        review_name = selector.xpath("//a[@class='author']/span/text()").extract()
+        review_id = selector.xpath("//a[@class='author']/@href").extract()
+        if review_id:
+            for index,id in enumerate(review_id):
+                review_id[index] = str(re.search('https://www.douban.com/people/(.*?)/',id).group(1))
+
+        if len(tryread) >= 4:
+            tryread = tryread[3]
 
         item['bookid'] = bookid
         item['title'] = title
         item['author'] = author
         item['image'] = image
-
 
         item['isbn13'] = isbn13
         item['publisher'] = publisher
@@ -82,5 +106,15 @@ class DouBan(CrawlSpider):
         item['summary'] = summary
         item['catalog'] = catalog
         item['lookcount'] = lookcount
+        if len(tryread) >= 4:
+            item['tryread'] = tryread
 
+        item['short_review_content'] = short_review_content
+        item['short_review_name'] = short_review_name
+        item['short_review_id'] = short_review_id
+
+        item['review_title'] = review_title
+        item['review_link'] = review_link
+        item['review_name'] = review_name
+        item['review_id'] = review_id
         yield item
